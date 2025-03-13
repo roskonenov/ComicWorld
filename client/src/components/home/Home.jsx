8d0858afc53b83fe19a346780a12ecb0af5786e9
Rosen <roskonenov@gmail.com>
2025-03-13 18:49:50 +0200

    add more sections on home screen

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,11 +1,26 @@
 import Section from "./section/Section";
-
 import homeStyle from "./Home.module.css"
 
+import { useEffect, useState } from "react";
+import comicService from "../../services/comicService";
+
+
 export default function Home() {
+    const [comics, setComics] = useState([]);
+    
+        useEffect(() => {
+            comicService.getAllComicInfos()
+                .then(setComics);
+        }, []);
+    
+        const latestComics = comics.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).slice(0, 5);
+        const discountedComics = comics.filter(comic => comic.oldPrice).slice(0, 5);
+        
+
     return (
         <main className={homeStyle.home}>
-            <Section />
+            <Section comics={latestComics} title="Latest comics"/>
+            <Section comics={discountedComics} title="Hot offers" />
         </main>
     );
 }
--- client/src/components/home/section/Section.jsx
+++ client/src/components/home/section/Section.jsx
@@ -1,25 +1,20 @@
-import { useEffect, useState } from "react";
+
 import ComicItem from "./comic-item/ComicItem";
 
 import styles from "./Section.module.css"
-import comicService from "../../../services/comicService";
 
-export default function Section() {
-    const [comics, setComics] = useState([]);
-
-    useEffect(() => {
-        comicService.getAllComicInfos()
-            .then(setComics);
-    }, []);
-  
+export default function Section({
+    comics,
+    title
+}) {
     return (
         <section className={styles['section-container']}>
             <div className={styles['title-container']}>
-                <h1 className={styles['section-title']}>Newest comics</h1>
+                <h1 className={styles['section-title']}>{title}</h1>
             </div>
             <div className={styles['list-container']}>
                 <ul className={styles.list}>
-                    {comics.map(comic => <ComicItem key={comic._id} {...comic}/>)}
+                    {comics.map(comic => <ComicItem key={comic._id} {...comic} />)}
                 </ul>
             </div>
         </section>
