7a09cf2130494829eb4cce80e7715420f371720d
roskonenov <roskonenov@gmail.com>
2025-04-27 18:56:55 +0300

    min change to logout

--- client/src/api/authenticationApi.js
+++ client/src/api/authenticationApi.js
@@ -65,7 +65,7 @@ export function useLogout() {
 
         fetchResource(`${baseUrl}/logout`, options)
             .then(() => toast.success('You have been logged out'))
-            .finally(userLogoutHandler());
+            .finally(() => userLogoutHandler());
 
         return () => abortController.abort();
     }, [accessToken, userLogoutHandler]);
